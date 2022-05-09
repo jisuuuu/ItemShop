8b31fa733fb70ad92805f73ce95c325ef7c7e34a
gjkl55 <gjkl55@naver.com>
2022-05-09 21:23:02 +0900

    [수정] 상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,9 +36,45 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능, Model Param 제거 가능, 이름 바꾸면 X
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model) {
+        //지정한 이름이 없으면 Class 이름에서 첫글자만 소문자로 바뀐 이름으로 모델에 넣음
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV4(Item item) {
+        //지정한 이름이 없으면 Class 이름에서 첫글자만 소문자로 바뀐 이름으로 모델에 넣음
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
